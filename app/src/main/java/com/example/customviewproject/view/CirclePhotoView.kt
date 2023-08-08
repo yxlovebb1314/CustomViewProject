17e6dab95df1996d3774cfc79209c9b5d705b262
bisheng <bisheng233@163.com>
2023-08-08 16:28:50 +0800

    1.通过遮罩效果实现简单的圆角图片 2.如何实现官方的遮罩效果

---
+++ app/src/main/java/com/example/customviewproject/view/CirclePhotoView.kt
@@ -0,0 +1,48 @@
+package com.example.customviewproject.view
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import com.example.customviewproject.R
+import com.example.newproject.ui.utils.px
+
+private val IMAGE_WIDTH = 300f.px
+private val IMAGE_PADDING = 0f
+private val XFERMODE = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)  //设置遮罩模式
+
+class CirclePhotoView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val bounds = RectF(IMAGE_PADDING, IMAGE_PADDING, IMAGE_PADDING + IMAGE_WIDTH, IMAGE_PADDING + IMAGE_WIDTH)
+
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        //设置离屏缓冲区域
+        val count = canvas.saveLayer(bounds, null)
+        canvas.drawOval(IMAGE_PADDING, IMAGE_PADDING, IMAGE_PADDING + IMAGE_WIDTH, IMAGE_PADDING + IMAGE_WIDTH, paint)
+        paint.xfermode = XFERMODE
+        canvas.drawBitmap(getAvatar(IMAGE_WIDTH.toInt()), IMAGE_PADDING, IMAGE_PADDING, paint)
+        paint.xfermode = null
+        canvas.restoreToCount(count)
+    }
+
+    private fun getAvatar(width: Int) : Bitmap {
+        val options = BitmapFactory.Options()
+        //预加载图片，只加载图片的参数
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeResource(resources, R.mipmap.messi, options)
+        options.inJustDecodeBounds = false
+        options.inDensity = options.outWidth
+        options.inTargetDensity = width
+        return BitmapFactory.decodeResource(resources, R.mipmap.messi, options)
+    }
+
+}
---
+++ app/src/main/java/com/example/customviewproject/view/XfermodeTestView.kt
@@ -0,0 +1,52 @@
+package com.example.customviewproject.view
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import com.example.newproject.ui.utils.px
+
+class XfermodeTestView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+
+    //离屏缓冲区域
+    private val bufferRect = RectF(50f.px, 0f, 200f.px, 150f.px)
+
+    private val circleBitmap =
+        Bitmap.createBitmap(150f.px.toInt(), 200f.px.toInt(), Bitmap.Config.ARGB_8888)
+    private val rectBitmap =
+        Bitmap.createBitmap(150f.px.toInt(), 200f.px.toInt(), Bitmap.Config.ARGB_8888)
+
+    init {
+        val canvas = Canvas()
+        //以圆形bitmap为画布，画出圆形
+        canvas.setBitmap(circleBitmap)
+        paint.color = Color.parseColor("#aa00cc")
+        canvas.drawOval(100f.px, 0f, 200f.px, 100f.px, paint)
+        //再以方形bitmap作为画布，画出方形
+        canvas.setBitmap(rectBitmap)
+        paint.color = Color.parseColor("#00aabb")
+        canvas.drawRect(50f.px, 50f.px, 150f.px, 150f.px, paint)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        //设置离屏缓冲，这个方式是比较耗费资源的，所以能设置多小，就设置多小
+        canvas.saveLayer(bufferRect, null)
+//        canvas.drawOval(100f.px, 0f, 200f.px, 100f.px, paint)
+        canvas.drawBitmap(circleBitmap,50f.px, 0f, paint)
+        paint.xfermode = xfermode
+//        canvas.drawRect(50f.px, 50f.px, 150f.px, 150f.px, paint)
+        canvas.drawBitmap(rectBitmap, 50f.px, 0f, paint)
+        paint.xfermode = null
+        canvas.restore()
+    }
+}
